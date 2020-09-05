e6e779134f4677a8821eb8b7521c1fc7261a29f7
jbsommers095 <40323408+jbsommers095@users.noreply.github.com>
2020-09-04 23:46:27 -0500

    Add files via upload

---
+++ Minesweeper/src/Number.java
@@ -0,0 +1,16 @@
+public class Number extends Tile{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Number(int name, int x, int y) {
+		super(name, x, y);
+	}
+	public Number(int name) {
+		super(name);
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
